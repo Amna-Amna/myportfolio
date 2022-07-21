2b03923b695d980adbdec880efc83a7813490918
Amna-Amna <mailtoamna4@gmail.com>
2022-07-21 16:04:34 +0500

    changes in name css

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,12 @@
 const megaContainer = document.querySelector(".mega-container")
 const container = document. querySelector(".container")
 
+
 megaContainer.addEventListener("mousemove", (e)=>{
     console.log(e.pageX , e.pageY)
-    xAxis = (window.innerWidth - e.pageX)/25
-    yAxis = (window.innerHeight - e.pageY)/25
-    container.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`
+    xAxis = (window.innerWidth - e.pageX)/20
+    yAxis = (window.innerHeight - e.pageY)/20
+    container.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 })
 
 megaContainer.addEventListener("mouseenter",(e)=>{
@@ -13,6 +14,6 @@ megaContainer.addEventListener("mouseenter",(e)=>{
 })
 
 megaContainer.addEventListener("mouseleave",(e)=>{
-    container.style.transition = "all 0.5s ease"
+    container.style.transition = "all 1s ease"
     container.style.transform = `rotateY(0) rotateX(0)`
 })
