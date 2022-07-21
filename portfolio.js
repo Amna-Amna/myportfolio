1999ac9836073f85c9640d3d0227f080aee8b9cf
Amna-Amna <mailtoamna4@gmail.com>
2022-07-21 17:01:07 +0500

    changes in css

--- portfolio.js
+++ portfolio.js
@@ -3,7 +3,6 @@ const container = document. querySelector(".container")
 
 
 megaContainer.addEventListener("mousemove", (e)=>{
-    console.log(e.pageX , e.pageY)
     xAxis = (window.innerWidth - e.pageX)/20
     yAxis = (window.innerHeight - e.pageY)/20
     container.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
@@ -17,3 +16,5 @@ megaContainer.addEventListener("mouseleave",(e)=>{
     container.style.transition = "all 1s ease"
     container.style.transform = `rotateY(0) rotateX(0)`
 })
+ console.log(window.innerWidth)
+ console.log(window.innerHeight)
