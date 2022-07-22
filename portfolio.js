efc2d3a40664fa492343dd4ac5db8ed31533e272
Amna-Amna <mailtoamna4@gmail.com>
2022-07-22 20:18:33 +0500

    added 3rd page

--- portfolio.js
+++ portfolio.js
@@ -1,25 +1,24 @@
 AOS.init({
-    offset :5,
-    duration: 2500
-})
+  offset: 2,
+  duration: 2500,
+});
 
-const megaContainer = document.querySelector(".mega-container")
-const container = document. querySelector(".container")
+const megaContainer = document.querySelector(".mega-container");
+const container = document.querySelector(".container");
 
+megaContainer.addEventListener("mousemove", (e) => {
+  xAxis = (window.innerWidth - e.pageX) / 20;
+  yAxis = (window.innerHeight - e.pageY) / 20;
+  container.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
 
-megaContainer.addEventListener("mousemove", (e)=>{
-    xAxis = (window.innerWidth - e.pageX)/20
-    yAxis = (window.innerHeight - e.pageY)/20
-    container.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-})
+megaContainer.addEventListener("mouseenter", (e) => {
+  container.style.transition = "none";
+});
 
-megaContainer.addEventListener("mouseenter",(e)=>{
-    container.style.transition = "none"
-})
-
-megaContainer.addEventListener("mouseleave",(e)=>{
-    container.style.transition = "all 1s ease"
-    container.style.transform = `rotateY(0) rotateX(0)`
-})
- console.log(window.innerWidth)
- console.log(window.innerHeight)
+megaContainer.addEventListener("mouseleave", (e) => {
+  container.style.transition = "all 1s ease";
+  container.style.transform = `rotateY(0) rotateX(0)`;
+});
+console.log(window.innerWidth);
+console.log(window.innerHeight);
